2bf363cde206e31f1a2a47547e48557cf9adc110
hieugc <hieu.phamgc@gmail.com>
2023-01-20 11:45:46 +0700

    update code

---
+++ DoAnTotNghiep/Data/Data.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using DoAnTotNghiep.Entity;
+
+namespace DoAnTotNghiep.Data
+{
+    public class DoAnTotNghiepContext: DbContext
+    {
+        public DoAnTotNghiepContext(DbContextOptions<DoAnTotNghiepContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<City> Cities { get; set; }
+        //public DbSet<District> Districts { get; set; }
+        //public DbSet<Ward> Wards { get; set; }
+        //public DbSet<User> Users { get; set; }
+        //public DbSet<House> Houses { get; set; }
+    }
+}
---
+++ DoAnTotNghiep/Entity/City.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DoAnTotNghiep.Entity
+{
+    [Table("City")]
+    public class City
+    {
+        [Key]
+        [Column("id")]
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Hãy điền tên thành phố")]
+        [MaxLength(100, ErrorMessage = "Tên thành phố tối đa 100 ký tự")]
+        [Column("name")]
+        public string Name { get; set; } = string.Empty;
+        [Column("lat")]
+        public float Lat { get; set; } = 0;
+        [Column("long")]
+        public float Long { get; set; } = 0;
+        [Column("count")]
+        public int Count { get; set; } = 0;
+    }
+}
